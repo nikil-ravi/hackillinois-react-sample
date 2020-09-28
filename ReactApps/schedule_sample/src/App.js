9476ac2db15fafd296f4dff12452bc48e31b5dc9
nikilr2 <nikilr2@illinois.edu>
2020-09-27 20:51:45 -0500

    made minor changes to style and structure of code

--- ReactApps/schedule_sample/src/App.js
+++ ReactApps/schedule_sample/src/App.js
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import AddToCalendar from 'react-add-to-calendar';
-import logo from './logo.svg';
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBrain, faChalkboardTeacher, faClock, faCoffee, faHamburger, faLaptop, faStar, faWifi } from '@fortawesome/free-solid-svg-icons'
+import { faChalkboardTeacher, faClock, faCoffee, faHamburger, faLaptop, faStar, faWifi } from '@fortawesome/free-solid-svg-icons'
 
-
+// Returns an event object that satisfies the format required for
+// adding to calendar
 function buildEvent(props) {
   var time1 = new Date(props.startTime * 1000);
   var time2 = new Date(props.endTime * 1000);
+
   let eventToReturn = {
     title: String(props.name),
     description: String(props.description),
-    location: 'Siebel Center for Computer Science',
+    location: 'Siebel Center for Computer Science', // default
     startTime: time1,
     endTime: time2
   }
@@ -20,13 +21,15 @@ function buildEvent(props) {
   return eventToReturn;
 }
 
+// returns the string-version of the time passed (as a date object) to it.
 function getTime(t) {
   var hr = t.getHours();
   var m = "0" + t.getMinutes();
-
   return hr + ':' + m.substr(-2)
 }
 
+// returns a string to display for the time as 'timeStart - timeEnd' if the times are
+// different, or just 'timeStartEnd' if start time and end time are same.
 function getTimeToDisplay(t1, t2) {
   if (getTime(t1) == getTime(t2)) 
     return getTime(t1);  
@@ -34,9 +37,10 @@ function getTimeToDisplay(t1, t2) {
     return getTime(t1) + ' - ' + getTime(t2);
 }
 
-function HackILEvent(props) {
-  var ele;
-  switch (props.type) {
+// Returns an icon to display, based on the type of the event.
+function getIcon(typeOfEvent) {
+  var ele; // icon to be displayed
+  switch (typeOfEvent) {
     case "WORKSHOP":
       ele = <FontAwesomeIcon icon={faLaptop}/>;
       break;
@@ -56,10 +60,18 @@ function HackILEvent(props) {
       ele = <FontAwesomeIcon icon={faWifi}/>;
       break;
   }
+
+  return ele;
+}
+
+function HackILEvent(props) {
+  
+  var iconToDisplay = getIcon(props.type);
+
   return (
   <div className = "HackILEvent">
     <div>
-      <h2>{props.eventProp.title} {ele}</h2>
+      <h2>{props.eventProp.title} {iconToDisplay}</h2>
       <p>{props.eventProp.description}</p>
       <h3><FontAwesomeIcon icon={faClock}/> {getTimeToDisplay(props.eventProp.startTime, props.eventProp.endTime)}</h3>
       <div className="calendar">
@@ -74,7 +86,6 @@ function HackILEvent(props) {
 
 function App() {
   const [events, setEvents] = useState([]);
-
   useEffect(() => {
     fetch("https://api.hackillinois.org/event/").then(res => res.json()).then(json => {
       setEvents(json.events)
@@ -89,10 +100,12 @@ function App() {
         <div className="events">
           {
             events.map(event => (
-            <div>
-              <HackILEvent eventProp = {buildEvent(event)} sponsor={event.sponsor} type = {event.eventType}/>
-            </div>
-          ))}
+              <div>
+                <HackILEvent eventProp = {buildEvent(event)} type = {event.eventType}/>
+              </div>
+            )
+          )
+          }
         </div>
     </div>
   );
